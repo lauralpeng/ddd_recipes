f78e78ac2a7009fa4f00be81227e4dd4b37c76eb
anonymous anonymous <lauralpeng@gmail.com>
2022-03-08 15:01:32 +0000

    4th draft

--- app/resources/combination_resource.rb
+++ app/resources/combination_resource.rb
@@ -3,7 +3,8 @@ class CombinationResource < ApplicationResource
   attribute :created_at, :datetime, writable: false
   attribute :updated_at, :datetime, writable: false
   attribute :ingredient_id, :integer
-  attribute :quantity, :integer
+  attribute :quantity, :float
+  attribute :unit, :string
   attribute :dish_id, :integer
 
   # Direct associations
